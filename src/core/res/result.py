ae857e833e3f66e11ed2ac23f761fdc975d90b39
mohamad.alturky <m799678@gmail.com>
2024-07-10 07:14:53 +0300

    adding result type

---
+++ src/core/res/result.py
@@ -0,0 +1,21 @@
+class Result:
+    def __init__(self):
+        self.error = None
+        self.value = None
+        self.is_success = False
+        
+    @staticmethod
+    def success(value = None):
+        result = Result()
+        result.value = value
+        result.is_success = True
+        return result
+    
+    @staticmethod
+    def failure(error):
+        result = Result()
+        result.error = error
+        result.is_success = False
+        return result
+    def is_failure(self):
+        return not self.is_success
