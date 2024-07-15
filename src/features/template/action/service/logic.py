46ce58a947d5735292ba9626a6c37afcef0f48fd
mohamad.alturky <m799678@gmail.com>
2024-07-15 03:39:23 +0300

    adding template service

--- src/features/template/action/service/logic.py
+++ src/features/template/action/service/logic.py
@@ -1,11 +1,11 @@
 from core.res.result import Result
-from ..res.response import Response
-from ..model.request import Request
+from ..models.types import LLMModelIn, LLMModelOut
 
 
+# the service class for this slice
 class Service:
     def __init__(self):
         pass
-    
-    def run(self, request : Request) -> Result:
-        return Result.success(Response(result=request.name))
+
+    def serve(self, request : LLMModelIn):
+        return Result.success(LLMModelOut(nameLLMModelOut=request.nameLLMModelIn))
