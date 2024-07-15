1112e8dfa8fae7c5f8034d0bc096d778e193e088
mohamad.alturky <m799678@gmail.com>
2024-07-15 04:57:58 +0300

    register pools_extraction_router in the app runner

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from features.template.action.router.endpoint import router as template_router
+from features.pools.extract.router.endpoint import router as pools_extraction_router
+
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
 
 app.include_router(template_router)
+app.include_router(pools_extraction_router)
