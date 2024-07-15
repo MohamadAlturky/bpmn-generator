527581730500b66ea7a0cedb214a74c42082260d
mohamad.alturky <m799678@gmail.com>
2024-07-15 03:24:16 +0300

    adding docs for settings

--- src/core/settings/env.py
+++ src/core/settings/env.py
@@ -1,4 +1,9 @@
+#####################################################
+## Contains The Constants of the settings sections ##
+#####################################################
+## ⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️ ##
+#####################################################
+
 OLLAMA_HOST = "OLLAMA_HOST"
 DEFAULT_MODEL_NAME = "DEFAULT_MODEL_NAME"
 AYA_MODEL_NAME = "AYA_MODEL_NAME"
-TASKS_FOLDER_PATH = "TASKS_FOLDER_PATH"
