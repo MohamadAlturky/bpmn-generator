86e955c6251eb2411ae41b79ed59d7f1e7a04651
mohamad.alturky <m799678@gmail.com>
2024-07-15 04:40:19 +0300

    add template models  docs

--- src/features/template/action/models/types.py
+++ src/features/template/action/models/types.py
@@ -1,15 +1,29 @@
 from pydantic import BaseModel
 
-# the input type of the service
+########################################
+## the input type of the service       #
+########################################
+
 class LLMModelIn(BaseModel):
     nameLLMModelIn: str
-    
 
-# the output type of the service
+#☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
+
+########################################
+## some inner type used by the service #
+########################################
+
+class LLMModelInner(BaseModel):
+    nameLLMModelInner: str
+
+#☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
+
+
+########################################
+## the output type of the service      #
+########################################
+
 class LLMModelOut(BaseModel):
     nameLLMModelOut: str
 
-
-# some inner type used by the service
-class LLMModelInner(BaseModel):
-    nameLLMModelInner: str
+#☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
