87900149c53ff0707f9f0717ee0bcb2c5d13345b
mohamad.alturky <m799678@gmail.com>
2024-07-15 03:39:59 +0300

    change the endpoint structure with a detailed  docs

--- src/features/template/action/router/endpoint.py
+++ src/features/template/action/router/endpoint.py
@@ -1,41 +1,100 @@
-## imports from lib
+#########################################
+## import libraries 📕📗📘📙📔📗📘📒📚##
+#########################################
+
+##🔗 from python
 from http import HTTPStatus
 from fastapi import APIRouter
 from fastapi import HTTPException
 
-## imports from application core
+##🔗 from application core
 from core.res.result import Result
 
-## imports from the same slice
+##🔗 from the same slice
 from ..service.logic import Service
-from ..model.request import Request
+from ..contracts.api import Request
 from ..validator.applier import validate
+from ..mapping.mapper import Mapper
+#☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
 
-## create the router
+#########################################
+## this will be changed for each slice ##
+#########################################
+## responsibility : create the router  ##
+#########################################
+
 router = APIRouter(
     prefix="/template"
 )
+#☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
 
-## define the endpoint
+#########################################
+## this will be changed for each slice ##
+#########################################
+## responsibility: define the endpoint ##
+#########################################
+
 @router.post("/action")
 def extract_pools(request:Request):
     
-    # step 1 : request validation
+    #########################################
+    ## First Step    🔍🔍🔍🔍            ##
+    #########################################
+    ## responsibility : request validation ##
+    #########################################
     validation_result : Result = validate(request)
     
     if validation_result.is_failure():
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,
                             detail=validation_result.error)
+    #☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
     
-    # step 2 : get the response from the service
+    
+    #########################################
+    ## Second Step    🔖🔖🔖🧷🧷🧷      ##
+    #########################################
+    ## responsibility : request mapping    ##
+    #########################################
+    
+    mapper = Mapper()
+    inputs = mapper.map_to_service(request)
+    #☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
+    
+    
+    #########################################
+    ## Third Step    🏃🏻🏃🏻🏃🏻🏃🏻🏃🏻🏃🏻🏃🏻🏃🏻    ##
+    #########################################
+    ## responsibility : run the service    ##
+    #########################################
+
     service = Service()
     try:
-        result : Result = service.run(request)
+        result = service.serve(inputs)
     except Exception:
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR,detail="error happend sorry")
+    #☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
     
-    # step 3 : return the appropriate response
+    #########################################
+    ## Fourth Step       ✅|❌            ##
+    #########################################
+    ## responsibility: respond to client   ##
+    #########################################
+    
     if result.is_failure():
             raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY,detail=result.error)
+
+    # return the mapped response
+    return mapper.map_from_service(result.value)
+    #☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️#
     
-    return result.value
+
+## NOTE 📋:
+###🔗 The Advantages🥳 of This design:
+    ##-💪🏻 respects single responsibility princible
+    ##-💪🏻 respects fail early pattern 
+    ##-💪🏻 considered open for extension close for modification 
+    ##-💪🏻 respects the High Cohesion princible between the components of the same module   
+    
+###🔗 The Disadvantages😓 of this design:
+    ##-👎🏽 considered High Coupled Module, so there is coupling between the components of the same module
+    ##-👎🏽 considered Less Reusable than clean architecture
