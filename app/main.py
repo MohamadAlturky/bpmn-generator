a1ad3c37da124936b0ea87c95845483b948e8164
mohamad.alturky <mohamad.alturky@hiast.edu.sy>
2024-08-14 18:02:29 +0300

    adding traces generation

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
-# from features.template.action.router.endpoint import router as template_router
-# from features.generate.reports.router.endpoint import router as reports_router
-# from features.generate.diagram.router.endpoint import router as diagram_router
-# from features.generate.direct.router.endpoint import router as direct_router
 from features.collaboration.generate_with_two.router.endpoint import router as collaboration_router
 from features.collaboration.generate_report_with_two.router.endpoint import router as collaboration_report_router
 from features.rephrasing.generate.router.endpoint import router as rephrasing_report_router
 from features.questions.generate.router.endpoint import router as questions_router
 from features.bpmn.generate.router.endpoint import router as bpmn_router
-# from features.groq.endpoint import router as groq_router
-# from features.groq.endpoint_collaboration import router as groq_with_collaboration_router
+from features.bpmn.generateV2.router.endpoint import router as bpmn_router_v2
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
@@ -26,17 +21,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# app.include_router(template_router)
-# app.include_router(reports_router)
-# app.include_router(diagram_router)
-# app.include_router(direct_router)
-# app.include_router(groq_router)
-# app.include_router(groq_with_collaboration_router)
-
 app.include_router(collaboration_router)
 app.include_router(collaboration_report_router)
 app.include_router(rephrasing_report_router)
-# app.include_router(rephrasing_report_router)
 app.include_router(questions_router)
 app.include_router(bpmn_router)
+app.include_router(bpmn_router_v2)
