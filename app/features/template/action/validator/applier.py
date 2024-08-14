e91c2fd227825eed1d7a89573abbc2cd00265ca3
mohamad.alturky <mohamad.alturky@hiast.edu.sy>
2024-08-14 10:17:08 +0300

    remove the template feature

--- app/features/template/action/validator/applier.py
+++
@@ -1,9 +0,0 @@
-from core.res.result import Result
-from ..contracts.api import Request
-
-
-def validate(request : Request) ->Result:
-    if request.nameRequest == "":
-        return Result.failure("the name should not be empty.")
-    
-    return Result.success()
